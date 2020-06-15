2884be7e2e44d926e87fb9458ccf9f71822c8e99
= <=>
2020-06-15 19:57:13 +0530

    Added checkout screen functionalities

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <div>
-        Food Ordering App
-    </div>,
-    document.getElementById('root')
+    <Controller/>,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
